4776b31d17bdf011d053bd0e80b616ef07208eb1
miguelcidoncha <cidoncha1996@gmail.com>
2023-08-24 16:23:12 +0200

    Add. Nav-Tabs

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,28 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import './Nav.css';
+import LogoMultiverse from '../../assets/img/Backgrounds/LogoMultiverse.png'
+
+function NavbarView() {
+  return (
+    <Navbar expand="lg" className="navbar-container">
+      <Container className='nav-container'>
+        <Navbar.Brand href="#home"><img src={LogoMultiverse} alt='Logo Multiverse' className='MultiverseLogo'></img></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#comics">Cómics</Nav.Link>
+            <Nav.Link href="#figuras">Figuras</Nav.Link>
+            <Nav.Link href="#funkopops">Funko Pops</Nav.Link>
+            <Nav.Link href="#posters">Pósters</Nav.Link>
+            <Nav.Link href="#camisetas">Camisetas</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarView;
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TabsHeader from './components/Tabs/Tabs.jsx'
+import NavbarView from './components/Nav/Nav'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <TabsHeader />
+    <NavbarView />
   </React.StrictMode>,
 )

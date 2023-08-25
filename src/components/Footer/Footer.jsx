9994d0997c86660b417a88b1b1de1e277860bbc1
laurabarrerogonzalez <laura.bgpr94@gmail.com>
2023-08-25 10:41:01 +0200

    Delete-Old-Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,25 +1,3 @@
-// import React from 'react';
-// import './footer.css'; // Importa los estilos CSS
-
-// function Footer() {
-//   return (
-//     <div className="footer-container">
-//       <div className="main-content">
-//         {/* Contenido principal de tu página */}
-//       </div>
-//       <div className="styled-footer">
-//         <p>&copy; 2023 Mi Sitio Web</p>
-//         <p>Contacto: ejemplo@email.com</p>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Footer;
-
-
-
-
 import React from 'react';
 import './footer.css';
 

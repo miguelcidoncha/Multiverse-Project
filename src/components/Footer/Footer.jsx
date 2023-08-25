86fffc307651ba4ef5ab8ffec84396ed2e057b43
laurabarrerogonzalez <laura.bgpr94@gmail.com>
2023-08-25 10:32:11 +0200

    Border-DownFooter-Text

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,65 @@
+// import React from 'react';
+// import './footer.css'; // Importa los estilos CSS
+
+// function Footer() {
+//   return (
+//     <div className="footer-container">
+//       <div className="main-content">
+//         {/* Contenido principal de tu página */}
+//       </div>
+//       <div className="styled-footer">
+//         <p>&copy; 2023 Mi Sitio Web</p>
+//         <p>Contacto: ejemplo@email.com</p>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Footer;
+
+
+
+
 import React from 'react';
-import './footer.css'; // Importa los estilos CSS
+import './footer.css';
 
 function Footer() {
   return (
     <div className="footer-container">
-      <div className="main-content">
-        {/* Contenido principal de tu página */}
+      <div className="column">
+        <h6>Contacto</h6>
+        <p>924 00 00 06 - Prado / Pedidos Web</p>
+        <p>612 612 356 - Calle Mérida</p>
+        <p>tienda@mundofriki.es</p>
+      </div>
+      <div className="column">
+        <h6>Redes Sociales</h6>
+        <div className="social-icons">
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-facebook"></i>
+          </a>
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-instagram"></i>
+          </a>
+        </div>
+      </div>
+      <div className="column">
+        <h6>Información</h6>
+        <ul>
+          <li>Quiénes somos</li>
+          <li>Condiciones de uso</li>
+          <li>Política de Cookies</li>
+          <li>Empleo</li>
+        </ul>
       </div>
       <div className="styled-footer">
-        <p>&copy; 2023 Mi Sitio Web</p>
-        <p>Contacto: ejemplo@email.com</p>
+        <p>&copy; 2023 Multiverse</p>
       </div>
     </div>
   );
 }
 
 export default Footer;
-
-

627585e930f2b749596169708571df7c30aa931c
miguelcidoncha <cidoncha1996@gmail.com>
2023-08-26 18:27:47 +0200

    Act.

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TabsHeader from './components/Tabs/Tabs.jsx'
 import NavbarView from './components/Nav/Nav.jsx'
+import NewCarousel from './components/Carousel/Carousel';
 import MainBody from './components/Mainbody/Mainbody';
 // import Cards from './components/Cards/Cards';
 import Footer from './components/Footer/Footer.jsx';
+
 
 
 
@@ -15,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <TabsHeader />
     <NavbarView />
+    <NewCarousel />
     <MainBody />
     {/* <Cards /> */}
     <Footer />

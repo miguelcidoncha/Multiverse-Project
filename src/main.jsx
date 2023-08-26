6fa2e461b28e03b61f3dec5f47629dcef35c0332
Javiser <jascjav@gmail.com>
2023-08-26 18:31:41 +0200

    carrousel

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import NavbarView from './components/Nav/Nav.jsx'
 import MainBody from './components/Mainbody/Mainbody';
 // import Cards from './components/Cards/Cards';
 import Footer from './components/Footer/Footer.jsx';
+import NewCarousel from './components/Carousel/Carousel';
 
 
 
@@ -15,7 +16,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <TabsHeader />
     <NavbarView />
+    <NewCarousel/>
     <MainBody />
+
     {/* <Cards /> */}
     <Footer />
   </React.StrictMode>,

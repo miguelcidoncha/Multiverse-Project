89f41b592c7d00b2af575df033401b059349f3f0
laurabarrerogonzalez <laura.bgpr94@gmail.com>
2023-08-29 17:47:40 +0200

    ty

--- src/components/Registro/Registro.jsx
+++ src/components/Registro/Registro.jsx
@@ -16,77 +16,106 @@
 
 
 import CardsHandler from '../../Handler/Cardshandler';
-// import "../WheelOfDoom/WheelOfDoom.css";
+import '../Registro/Registro.css';
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
 
 export default function Registro() {
-  const [cardCustomer, setcardCustomer] = useState([]);
+  const [cardCustomer, setCardCustomer] = useState([]);
+  const [selectedFile, setSelectedFile] = useState(null); // Estado para la imagen seleccionada
+
   useEffect(() => {
     getData();
   }, []);
+
   const getData = async () => {
     const data = await CardsHandler.loadCards();
-    setcardCustomer(data);
+    setCardCustomer(data);
   };
+
   const handlerAdd = async () => {
     try {
-      // Datos del nuevo elemento a agregar (reemplaza con los valores adecuados)
-      const newCardCustomer = {
-        Foto: 'https://cdn.pixabay.com/photo/2016/04/01/09/34/american-1299445_640.png',
-        Description: 'Escultura 2',
-        Precio: '2000',
-        Tipo: 'Escultura',
-      };
-      // Realizar la solicitud POST para agregar el elemento a la base de datos (API)
-      const response = await axios.post('http://localhost:3000/Cards', newCardCustomer);
-      // Obtener el nuevo elemento con el ID generado automáticamente por el servidor
+      const formData = new FormData();
+      formData.append('Foto', selectedFile);
+      formData.append('Description', 'Nueva descripción');
+      formData.append('Precio', 'Nuevo precio');
+      formData.append('Tipo', 'Nuevo tipo');
+
+      const response = await axios.post('http://localhost:3000/Cards', formData);
       const addedCardCustomer = response.data;
-      // Actualizar el estado local para reflejar los cambios
-      setcardCustomer([...cardCustomer, addedCardCustomer]);
+      setCardCustomer([...cardCustomer, addedCardCustomer]);
+      setSelectedFile(null); // Reiniciar la selección de archivo
     } catch (error) {
       console.error('Error al agregar:', error);
     }
   };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedFile(file);
+  };
+
+  const handleUpdateImage = async (id) => {
+    if (selectedFile) {
+      try {
+        const formData = new FormData();
+        formData.append('Foto', selectedFile);
+
+        const response = await axios.patch(`http://localhost:3000/Cards/${id}`, formData);
+        const updatedCardCustomer = response.data;
+        const updatedCards = cardCustomer.map(card => {
+          if (card.id === id) {
+            return updatedCardCustomer;
+          }
+          return card;
+        });
+        setCardCustomer(updatedCards);
+        setSelectedFile(null); // Reiniciar la selección de archivo
+      } catch (error) {
+        console.error('Error al actualizar imagen:', error);
+      }
+    }
+  };
+
   const handleDelete = async (id) => {
     try {
       await axios.delete(`http://localhost:3000/Cards/${id}`);
       const updatedCardCustomer = cardCustomer.filter((item) => item.id !== id);
-      setcardCustomer(updatedCardCustomer);
+      setCardCustomer(updatedCardCustomer);
     } catch (error) {
       console.error('Error al eliminar:', error);
     }
   };
+
   return (
     <main id="background">
       <div id="art-customer-container">
-        {Array.from({ length: Math.ceil(cardCustomer.length / 5) }).map(
-          (_, rowIndex) => (
-            <div key={rowIndex} className="art-customer-group">
-              {cardCustomer
-                .slice(rowIndex * 5, (rowIndex + 1) * 5)
-                .map((item) => (
-                  <div key={item.id} className="art-customer">
-                    <div>
-                    <img src={item.Foto} alt="Imagen de arte" width={250} height={315}/>
-                    </div>
-                    <h2>{item.Description}</h2>
-                    <p><strong>Precio:</strong> {item.Precio}</p>
-                    <p><strong>Tipo:</strong> {item.Tipo}</p>
-                    <div className="button-container">
-                    <button className="delete-button" onClick={() => handleDelete(item.id)}>
-                        <img width={30} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLsJK7RvQtvRfpAW04KrAo7wBvW_7W9qhUqIgC8fYgtfjrSLRC5aXDjuwXRirrWxllkak&usqp=CAU" alt="Eliminar" />
-                    </button>
-                    <button className="add-button" onClick={handlerAdd}>
-                    <img width={30} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfj4CtfwLAx0n7ZfQWG08dsI7B6tRiYlabiUteYJU&s" alt="Agregar" />
-                    </button>
-                    </div>
-                  </div>
-                ))}
-            </div>
-          )
-        )}
+        {/* Resto del código para mostrar las cards */}
+      </div>
+      <div className="add-card-form">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={(e) => handleFileChange(e)}
+        />
+        <button className="add-button" onClick={handlerAdd}>
+          Agregar Nueva Card
+        </button>
+      </div>
+      <div className="update-image-form">
+        <h3>Modificar Imagen</h3>
+        <select onChange={(e) => setSelectedFile(e.target.value)}>
+          <option value={null}>Seleccionar Imagen</option>
+          {cardCustomer.map(card => (
+            <option key={card.id} value={card.Foto}>
+              {card.Description}
+            </option>
+          ))}
+        </select>
+        <button className="update-button" onClick={() => handleUpdateImage(selectedFile)}>
+          Actualizar Imagen
+        </button>
       </div>
     </main>
   );

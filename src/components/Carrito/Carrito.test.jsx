ec784e377c5aed1047b597d2f107f595f6a36b10
rebecapoma6 <mrebecapoma.6@gmail.com>
2023-09-04 18:22:25 +0200

    delete test carrito

--- src/components/Carrito/Carrito.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Carrito from './Carrito';
-
-test('renders Carrito component', () => {
-  render(<Carrito />);
-  const element = screen.getByText('Carrito');
-  expect(element).toBeInTheDocument();
-});
